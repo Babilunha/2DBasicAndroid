aac4982efe21fed1e8e8efba943f9e9cc608ce55
Maksym Babilunha <babilunha2@gmail.com>
2020-06-02 12:19:13 +0300

    Initial Upload

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Numerics;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    public UnityEngine.Vector3 offset; 
+
+    private void LateUpdate()
+    {
+        UnityEngine.Vector3 desiredPosition = target.position + offset;
+        UnityEngine.Vector3 smoothedPosition = UnityEngine.Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+
+
+}
